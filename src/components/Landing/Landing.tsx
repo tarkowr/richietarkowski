b9acbd6de7350d93a5e1b8f01747fae9b3377c3d
Richie Tarkowski <tarkowr@gmail.com>
2026-05-22 23:19:34 -0500

    cleanup css

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -11,22 +11,24 @@ export const Landing = () => {
 
   return (
     <div className='landing'>
-      <div className='name'>Richie Tarkowski</div>
-      <div className='title'>
-        <span>Software engineer</span>
-        <span className='divider'>|</span>
-        <span>Founder of Versify</span>
-        <span className='divider'>|</span>
-        <span>Follower of Christ</span>
-      </div>
-      <div className='links'>
-        <LinkCard className='link-linkedin' link='https://www.linkedin.com/in/richie-tarkowski-273238155'
-          icon={<i className='fa fa-linkedin'> </i>} onMouseOver={() => setLinkLabel('LINKEDIN')} onMouseOut={resetLinkLabel} />
-        <LinkCard className='link-github' link='https://github.com/tarkowr'
-          icon={<i className='fa fa-github'> </i>} onMouseOver={() => setLinkLabel('GITHUB')} onMouseOut={resetLinkLabel} />
-        <LinkCard className='link-mail' link='mailto:tarkowr@gmail.com'
-          icon={<i className='fa fa-envelope-o'> </i>} onMouseOver={() => setLinkLabel('EMAIL')} onMouseOut={resetLinkLabel} />
-        <div className='link-hover-label'>{linkLabel}</div>
+      <div className='wrapper'>
+        <div className='name'>Richie Tarkowski</div>
+        <div className='title'>
+          <span>Software engineer</span>
+          <span className='divider'>|</span>
+          <span>Founder of Versify</span>
+          <span className='divider'>|</span>
+          <span>Follower of Christ</span>
+        </div>
+        <div className='links'>
+          <LinkCard className='link-linkedin' link='https://www.linkedin.com/in/richie-tarkowski-273238155'
+            icon={<i className='fa fa-linkedin'> </i>} onMouseOver={() => setLinkLabel('LINKEDIN')} onMouseOut={resetLinkLabel} />
+          <LinkCard className='link-github' link='https://github.com/tarkowr'
+            icon={<i className='fa fa-github'> </i>} onMouseOver={() => setLinkLabel('GITHUB')} onMouseOut={resetLinkLabel} />
+          <LinkCard className='link-mail' link='mailto:tarkowr@gmail.com'
+            icon={<i className='fa fa-envelope-o'> </i>} onMouseOver={() => setLinkLabel('EMAIL')} onMouseOut={resetLinkLabel} />
+          <div className='link-hover-label'>{linkLabel}</div>
+        </div>
       </div>
     </div>
   );
